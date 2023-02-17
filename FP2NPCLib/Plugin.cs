697fc7d5cb017cb697b692dff39cddf870f749e8
Kuborros <kubo@catbat.rocks>
2023-02-17 22:42:16 +0100

    Added enums to simplify species and home selection. Added alt method to register the npc using enums.

--- FP2NPCLib/Plugin.cs
+++ FP2NPCLib/Plugin.cs
@@ -9,6 +9,45 @@ using UnityEngine;
 
 namespace FP2NPCLib
 {
+
+    public enum FPCharacterSpecies
+    {
+        UNKNOWN = 0,
+        CAT = 1,
+        BIRD = 2,
+        BEAR = 3,
+        PANDA = 4,
+        RED_PANDA = 5,
+        MONKEY = 6,
+        DOG = 7,
+        DEER = 8,
+        FOX = 9,
+        RABBIT = 10,
+        DRAGON = 11,
+        BAT = 12,
+        BOAR = 13,
+        OTTER = 14,
+        RACOON = 15,
+        GOAT = 16,
+        HEDGEHOG = 17,
+        REPTILIAN = 18,
+        //Number 19 is missing in the game
+        MOUSE = 20,
+        FERRET = 21,
+        PLANT = 22,
+        ROBOT = 23,
+        PANGOLIN = 24,
+    }
+
+    public enum FPCharacterHome
+    {
+        UNKNOWN = 0,
+        SHANG_TU = 1,
+        SHANG_MU = 2,
+        SHUIGANG = 3,
+        PARUSA = 4
+    }
+
     [BepInPlugin("000.kuborro.plugins.fp2.npclib", "FP2NPCLib", "1.0.0")]
     public class FP2NPCLib : BaseUnityPlugin
     {
@@ -27,6 +66,11 @@ namespace FP2NPCLib
             harmony.PatchAll(typeof(PatchStageRegisterNPC));
 
         }
+        public static bool registerNPC(string uID, string Name, string Scene, GameObject Prefab, FPCharacterSpecies Species = FPCharacterSpecies.UNKNOWN, FPCharacterHome Home = FPCharacterHome.UNKNOWN, int DialogueTopics = 1)
+        {
+            return registerNPC(uID, Name, Scene, Prefab, (int)Species, (int)Home, DialogueTopics);
+        }
+
         public static bool registerNPC(string uID, string Name, string Scene, GameObject Prefab, int Species = 0, int Home = 0, int DialogueTopics = 1)
         {
             if (!HubNPCs.ContainsKey(uID))
